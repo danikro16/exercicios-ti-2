1cf23190453924f1bef7aed3cad94c216bb2fa05
Daniel Reis <danieldosrreis1@gmail.com>
2025-08-25 17:20:12 -0300

    exercicio1

---
+++ exercicio1/src/soma1/Soma.java
@@ -0,0 +1,14 @@
+package soma1;
+
+import java.util.Scanner;
+
+public class Soma {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x, y;
+		x = sc.nextInt();
+		y = sc.nextInt();
+		System.out.println(x+y);
+		sc.close();
+	}
+}
